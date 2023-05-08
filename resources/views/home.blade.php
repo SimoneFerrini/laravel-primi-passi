51f28bccf7b24c2b8a336ea73f8985291ac4f437
SimoneFerrini <ferrini.simone4@gmail.com>
2023-05-08 16:08:22 +0200

    error with route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel-primi-passi</title>
 
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
 </head>
 <body>
     Hello World!!!
